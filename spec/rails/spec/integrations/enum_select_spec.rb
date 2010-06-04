c7bcc4181a2bd3da83763cf67d9a897d17d5ae49
Lailson Bandeira <lailson@guava.com.br>
2010-06-03 22:49:32 -0300

    Changing relative require on test to make compatible with Rails 1.9.2.

--- spec/rails/spec/integrations/enum_select_spec.rb
+++ spec/rails/spec/integrations/enum_select_spec.rb
@@ -1,4 +1,4 @@
-require File.dirname(__FILE__) + '/../spec_helper.rb'
+require File.expand_path(File.join(File.dirname(__FILE__), '../spec_helper.rb'))
 
 shared_examples_for "enum_select form" do
 	it "should have select boxes for gender, status and degree initially set blank" do
