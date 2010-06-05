04a84270371afdd805f3ea6f512686aff8f9204e
jeffp <jpatmon@gmail.com>
2010-06-04 17:31:47 -0700

    alters version is rails spec environment file

--- spec/rails/config/environment.rb
+++ spec/rails/config/environment.rb
@@ -1,7 +1,7 @@
 # Be sure to restart your server when you modify this file
 
 # Specifies gem version of Rails to use when vendor/rails is not present
-RAILS_GEM_VERSION = ['>=2.3.0', '< 3.0.0'] unless defined? RAILS_GEM_VERSION
+RAILS_GEM_VERSION = '< 2.4.0' unless defined? RAILS_GEM_VERSION
 
 # Bootstrap the Rails environment, frameworks, and default configuration
 require File.join(File.dirname(__FILE__), 'boot')
